fbdcd7355f99cd28d8ff6a68cd844ed5aebda141
Filipe Brochier <filipe.brochier@gmail.com>
2021-08-27 15:08:03 -0300

    Adiciona bonus1.js

---
+++ 1-fundamentals/block-4/day-2/bonus1.js
@@ -0,0 +1,11 @@
+let array = [2, 9, 19, 70, 100, 8, 5, 3, 35, 27];
+for (let index = 1; index < array.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (array[index] < array[secondIndex]) {
+      let position = array[index];
+      array[index] = array[secondIndex];
+      array[secondIndex] = position;
+    }
+  }
+}
+console.log(array);

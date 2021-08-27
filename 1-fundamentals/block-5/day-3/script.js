17893ad66a722e1f945989a276b3b069f8a1bf9b
Filipe Brochier <filipe.brochier@gmail.com>
2021-08-27 18:12:35 -0300

    Corrige exercicios

--- 1-fundamentals/block-5/day-3/script.js
+++ 1-fundamentals/block-5/day-3/script.js
@@ -1,3 +1,7 @@
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const divButton = document.querySelector('.buttons-container');
+const button = document.createElement('button');
+
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');

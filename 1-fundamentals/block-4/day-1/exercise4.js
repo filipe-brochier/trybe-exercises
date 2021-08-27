6d0def328fcead5d451bba1ef43a0b63f1f92097
Filipe Brochier <filipe.brochier@gmail.com>
2021-08-27 14:58:02 -0300

    Adiciona exercise4.js

---
+++ 1-fundamentals/block-4/day-1/exercise4.js
@@ -0,0 +1,9 @@
+/*
+Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+*/
+
+const num = 13;
+
+if (num > 0) { console.log('positive'); }
+if (num < 0) { console.log('negative'); }
+if (num === 0) { console.log('zero'); }

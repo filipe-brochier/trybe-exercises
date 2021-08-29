c00a50d9a6b91ac52fc26e60c950986ed0a42d5e
Filipe Brochier <filipe.brochier@gmail.com>
2021-08-29 16:31:52 -0300

    Exercicio concluido, script.js com localStorage

--- 1-fundamentals/block-5/day-4/script.js
+++ 1-fundamentals/block-5/day-4/script.js
@@ -4,7 +4,7 @@ document.querySelector('#bgcolor').addEventListener('click', () => {
   } else {
   document.body.style.backgroundColor = 'grey';
   }
-  // localStorage.setItem('bgcolor', document.body.style.backgroundColor);
+  localStorage.setItem('bgcolor', document.body.style.backgroundColor);
 });
 
 document.querySelector('#color').addEventListener('click', () => {
@@ -13,6 +13,7 @@ document.querySelector('#color').addEventListener('click', () => {
   } else {
   document.body.style.color = 'grey';
   }
+  localStorage.setItem('color', document.body.style.color);
 });
 
 document.querySelector('#size').addEventListener('click', () => {
@@ -21,6 +22,7 @@ document.querySelector('#size').addEventListener('click', () => {
   } else {
     document.body.style.fontSize = '2rem';
   }
+  localStorage.setItem('fontSize', document.body.style.fontSize);
 });
 
 document.querySelector('#spacing').addEventListener('click', () => {
@@ -29,6 +31,7 @@ document.querySelector('#spacing').addEventListener('click', () => {
  } else {
   document.body.style.lineHeight = '2rem';
  }
+ localStorage.setItem('lineHeight', document.body.style.lineHeight);
 });
 
 document.querySelector('#family').addEventListener('click', () => {
@@ -37,12 +40,13 @@ document.querySelector('#family').addEventListener('click', () => {
   } else {
     document.body.style.fontFamily = 'Arial'
   }
+  localStorage.setItem('fontFamily', document.body.style.fontFamily);
 });
 
 window.onload = () => {
-  // document.body.style.backgroundColor = localStorage.getItem('bgcolor');
-  // localStorage.getItem();
-  // localStorage.getItem();
-  // localStorage.getItem();
-  // localStorage.getItem();
+  document.body.style.backgroundColor = localStorage.getItem('bgcolor');
+  document.body.style.color = localStorage.getItem('color');
+  document.body.style.fontSize = localStorage.getItem('fontSize');
+  document.body.style.lineHeight = localStorage.getItem('lineHeight');
+  document.body.style.fontFamily = localStorage.getItem('fontFamily');
 }

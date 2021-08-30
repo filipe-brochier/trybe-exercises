913b39f73aa361daffcd014d5dd18a0ede880de2
Filipe Brochier <filipe.brochier@gmail.com>
2021-08-30 18:25:57 -0300

    Resolve exercise1.js

--- 1-fundamentals/block-9/day-2/part1/exercise1.js
+++ 1-fundamentals/block-9/day-2/part1/exercise1.js
@@ -1,9 +1,14 @@
-// apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
-  // Adicionar lógica aqui!
-  await fetch
+const fetchJoke = async () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  const object = await fetch(API_URL, myObject)
+  const objectJSON = await object.json()
+  document.querySelector('h2').innerText = objectJSON.joke;
 };
 
 window.onload = () => fetchJoke();

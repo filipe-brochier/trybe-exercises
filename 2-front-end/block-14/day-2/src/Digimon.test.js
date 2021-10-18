a9e696b8a40c5d4a2286fc2429cad6346f7c0c71
Filipe Brochier <filipe.brochier@gmail.com>
2021-10-18 11:56:47 -0300

    Testes em digimon.test.js

--- 2-front-end/block-14/day-2/src/Digimon.test.js
+++ 2-front-end/block-14/day-2/src/Digimon.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Digimon from './Digimon';
 
 describe('Teste da tela do Digimon', () => {
-  it('renders Digimon', async () => {
+  beforeEach(() => {
+    const digimon = {
+      name: 'Agumon',
+      level: 'Rookie',
+      img: 'https://digimon.shadowsmith.com/img/agumon.jpg',
+    };
+    render(<Digimon digimon={ digimon } />)
+  });
 
+  it('should have a heading', () => {
+    const heading = screen.getByRole('heading', { name: 'Agumon', level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should have level info', () => {
+    const level = screen.getByText(/level: rookie/i)
+    expect(level).toBeInTheDocument();
+  });
+  it('should have an image', () => {
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://digimon.shadowsmith.com/img/agumon.jpg');
   });
 });
